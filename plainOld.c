ad081798df4ffbb771b6348a6cd5dc24e34babec
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-06-28 19:07:38 +0400

    Update plainOld.c

--- plainOld.c
+++ plainOld.c
@@ -4,6 +4,7 @@
 #include "curl_data.h"													// gcc plainOld.c curl_data.c $(pkg-config --libs --cflags libcurl) -o a.out
 #include "pointers.h"													// gcc plainOld.c pointers.c -o a.out
 #include "pointer_array.h"												// gcc plainOld.c pointer_array.c -o a.out
+#include "read_ext.h"													// gcc plainOld.c read_ext.c -o a.out
 
 int main(int argc, char const *argv[]) {
 //	recursionCall(5);
@@ -12,16 +13,7 @@ int main(int argc, char const *argv[]) {
 //	curl_test();
 //	pointerSample();
 //	pointerMatrix();
-
-/*	int junk[ROWS][COLS] = {
-		{2, 4, 6, 8},
-		{3, 5, 7, 9},
-		{12, 10, 8, 6}
-	};
-	sum_rows(junk, ROWS);
-	sum_cols(junk, ROWS);
-	printf("Summ of all elements = %d\n", sum2d(junk, ROWS));
-*/
+//	read_from_file();
 
 	return 0;
 }
